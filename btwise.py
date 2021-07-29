c59b9a2e27dc364e26b800a54baf74f2f169b2ec
Aman <30496447+aman2575@users.noreply.github.com>
2021-07-29 11:15:31 +0530

    Add files via upload

---
+++ btwise.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+
+img1 = np.zeros((250,500,3), np.uint8)
+img1 = cv2.rectangle(img1,(200,0), (300,100),(255,255,255),-1)
+img2 = cv2.imread("image.jpg")
+img2 = cv2.resize(img2,(500,250))
+#bitAnd = cv2.bitwise_and(img2,img1);
+#bitOr = cv2.bitwise_or(img2,img1)
+#bitXor = cv2.bitwise_xor(img1,img2);
+bitNot1 = cv2.bitwise_not(img1);
+bitNot2 = cv2.bitwise_not(img2);
+cv2.imshow("img1",img1)
+cv2.imshow("img2",img2)
+#cv2.imshow('bitAnd',bitAnd)
+#cv2.imshow('bitOr',bitOr)
+#cv2.imshow('bitXor',bitXor)
+cv2.imshow('bitNot1',bitNot1)
+cv2.imshow('bitNot2',bitNot2)
+print("Size of image1:",img1.shape)
+print("Size of image2:",img2.shape)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
